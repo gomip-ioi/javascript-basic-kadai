93bf4305f3e4755acb91109951e7362b339cabec
gomip <145749169+gomip-ioi@users.noreply.github.com>
2024-02-29 11:31:32 +0900

    Add files via upload

---
+++ kadai_005(2)/bmi.js
@@ -0,0 +1,30 @@
+//体重
+let bodywight;
+
+bodywight = '体重';
+
+let wight;
+
+wight = '68';
+
+console.log(bodywight);
+console.log(wight);
+
+//身長
+let bodyhight;
+
+bodyhight = '身長';
+
+let hight;
+
+hight = '1.7';
+
+console.log(bodyhight);
+console.log(hight);
+
+//bmi
+let bmi;
+
+bmi = wight / (hight * hight);
+
+console.log(bmi);
